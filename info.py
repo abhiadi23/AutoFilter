4cf12c6e4d3263dfda4076e56f6300891dbea198
[Aᴅɪᴛʏᴀ Aʙʜɪɴᴀᴠ] <abhinavaditya035@gmail.com>
2025-05-11 12:21:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', " "56784675"))
+API_ID = int(environ.get('API_ID', "56784675"))
 API_HASH = environ.get('API_HASH', "")
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
